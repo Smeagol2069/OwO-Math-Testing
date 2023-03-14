d7e13180f1a311c57de506fc6110926002d1e072
Smeagol2069 <Thesmeagol360@gmail.com>
2023-03-13 21:44:15 -0400

    Now can process data and find the stat sum

--- src/weaponmath.js
+++ src/weaponmath.js
@@ -9,5 +9,14 @@ const data = {   id: '210177401064390658',
     pstat: '58,88'
  }
 
+function calculate(data) { // Returns an array, 0 index is best % without crit, 1 index is best % with crit
+		const wid = data.wid;
+		let output = []
 
-console.log(widStats[1])
+		const statSum = 1 + .5 + .5
+		const numberStats = widStats[wid].numstats
+		output[0] = (100*statSum / numberStats)
+		output[1] = (100*(statSum + 1) / (numberStats + 1))
+	return output	
+}
+console.log(calculate(data))
